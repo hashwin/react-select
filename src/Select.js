b3c1cb32375d73fda86fa2fa5a5caf7fcf009a8e
Ashwin Hegde <ashwin.hegde12@gmail.com>
2018-03-08 14:22:29 +0530

    Scroll menu container into view

--- src/Select.js
+++ src/Select.js
@@ -184,7 +184,7 @@ class Select extends React.Component {
 		if (this.props.scrollMenuIntoView && this.menuContainer) {
 			const menuContainerRect = this.menuContainer.getBoundingClientRect();
 			if (window.innerHeight < menuContainerRect.bottom + this.props.menuBuffer) {
-				window.scrollBy(0, menuContainerRect.bottom + this.props.menuBuffer - window.innerHeight);
+				this.menuContainer.scrollIntoView();
 			}
 		}
 		if (prevProps.disabled !== this.props.disabled) {
